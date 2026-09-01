df48e0523a8ee43d51612a9c68b5571400f2c491
elCris91 <cr.retamalesr@duocuc.cl>
2026-08-31 21:25:12 -0400

    docs: actualizar README y aislar microservicio Clientes

--- prueba3-master/InventarioF/src/main/java/Anfri/InventarioF/Repository/InventarioFRepository.java
+++
@@ -1,8 +0,0 @@
-package Anfri.InventarioF.Repository;
-import Anfri.InventarioF.Model.InventarioF;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InventarioFRepository extends JpaRepository<InventarioF, Integer> {
-}
